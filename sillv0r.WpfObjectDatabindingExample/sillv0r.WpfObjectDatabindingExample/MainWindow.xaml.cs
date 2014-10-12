3f3ec7ed8476983e7747cd7c2954dd375c7a8d40
Michael Millies <mmillies@gmail.com>
2014-10-12 12:42:25 -0500

    initial commit, testing out the repo

---
+++ sillv0r.WpfObjectDatabindingExample/sillv0r.WpfObjectDatabindingExample/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace sillv0r.WpfObjectDatabindingExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public class ViewModel
+        {
+            public ViewModel()
+            {
+                this.Ingredients = new List<string>();
+                this.Ingredients.AddRange(new string[] {"Eggs", "Bread", "Celery" });
+            }
+            public List<string> Ingredients { get; set; }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.DataContext = new ViewModel();
+        }
+    }
+}
