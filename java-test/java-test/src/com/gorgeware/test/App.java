a06b7fb0f04c61ea278ff2655350c6193eb429e7
Michael Millies <mmillies@gmail.com>
2014-10-26 11:50:43 -0500

    added test java app

---
+++ java-test/java-test/src/com/gorgeware/test/App.java
@@ -0,0 +1,9 @@
+package com.gorgeware.test;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+	}
+	
+}
---
+++ java-test/java-test/src/com/gorgeware/test/SomeTestClass.java
@@ -0,0 +1,5 @@
+package com.gorgeware.test;
+
+public class SomeTestClass {
+
+}
